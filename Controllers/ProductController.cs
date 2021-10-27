19fce2a5c0b4f21492b39aafeab487374069feb5
1703826 <gmrs99.silva@gmail.com>
2021-10-27 11:10:41 +0100

    Alterado o layout dos produtos

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ISportsStoreRepository repository;
+        public ProductController(ISportsStoreRepository repository)
+        {
+            this.repository = repository;
+        }
+            public IActionResult Index()
+        { 
+            return View(repository.Products);
+        }
+    }
+}
---
+++ Models/FakeSportsStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public class FakeSportsStore : ISportsStoreRepository
+    {
+        public IEnumerable<Product> Products => new List<Product>
+        {
+            new Product
+            {
+                Name = "Football",
+                Price = 25
+            },
+            new Product
+            {
+                Name = "PranchaSurf",
+                Price = 175
+            },
+            new Product
+            {
+                Name = "RunningShoes",
+                Price = 40
+            }
+        };
+    }
+}
